610af2cb2a9b629c2cd986c40abe9d179790aec2
Tiago António <aluno20000@ipt.pt>
2021-02-10 12:24:01 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Segmentation
+# Segment and downsampling of audio files returned by Spleeter using [fluent-ffmpeg](https://www.npmjs.com/package/fluent-ffmpeg).
+
+This service is connected with ['Manager'](https://github.com/mer-team/Tests/blob/rabbit-manager/Manager/manager.js) service through [RabbitMQ](https://www.rabbitmq.com/). 
 Segments an audio file into smaller files with 30 seconds length and 15 seconds of overlapping. This files are saved with one audio channel only and 22500 Hz of frequency.
+
+Run `node segmentation.js`
+
+## Input through RabbitMQ
+
+The input is the videoID which is the folder that contains separated audio files.
+
+## Output
+
+Segmented audio files.
+
+Through RabbitMQ:
+```javascript
+{ Service: 'Segmentation', Result: { vID: 'vID' } }
+```
---
+++ src/segmentation.js
@@ -0,0 +1,67 @@
+var ffmpeg = require('fluent-ffmpeg');
+var amqp = require('amqplib/callback_api');
+
+segmentation = async (channel, folder, audio) => {
+    var path = "/vagrant/SourceSeparation/Spleeter/Output/" + folder + "/";
+    // file metadata
+    ffmpeg.ffprobe(path + audio + '.wav', async function (err, metadata) {
+        var inputSec = 0;
+        var duration = metadata.format.duration;
+        // Round a number upward to its nearest integer:
+        var nFiles = Math.ceil(duration / 15);
+        // console.log(nFiles)
+        for (let i = 1; i <= nFiles; i++) {
+            var command = ffmpeg(path + audio + '.wav')
+                .seekInput(inputSec)
+                .duration(30)
+                .withAudioChannels(1)
+                .withAudioFrequency(22500)
+                .on('error', function (err) {
+                    console.log('An error occurred: ' + err.message);
+                })
+                .on('end', function () {
+                    // true when i is the last file and audio is vocals
+                    // so it just trigger once
+                    if (i == nFiles && audio == "vocals") {
+                        var q = 'management';
+                        var toSend = {
+                            Service: "Segmentation",
+                            Result: { "vID": folder }
+                        }
+                        channel.sendToQueue(q, Buffer.from(JSON.stringify(toSend)));
+                        console.log(" [x] Sent %s to %s", toSend, q);
+                    }
+                })
+                .saveToFile(path + audio + i + ".wav");
+
+            inputSec = inputSec + 15;
+        }
+    });
+}
+
+amqp.connect('amqp://localhost', async function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(async function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'segmentation';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function (msg) {
+            var folder = msg.content.toString()
+            console.log(" [x] Received %s", folder);
+            //using source separation with 2 stems (vocals and accompaniment) 
+            segmentation(channel, folder, 'accompaniment');
+            segmentation(channel, folder, 'vocals');
+        }, {
+            noAck: true
+        });
+    });
+});
